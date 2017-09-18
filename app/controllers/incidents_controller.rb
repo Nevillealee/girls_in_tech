496c70a4b922930a769555d371919efa0f73e6e5
nevillealee <nevillealee@gmail.com>
2017-09-18 03:55:56 +0000

    creates show action in incidents controller for post sign in page

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,4 +1,14 @@
 class IncidentsController < ApplicationController
     before_action :authenticate_user!
-
+    
+    def show
+         @incident = Incident.find_by_id(params[:id])
+        return render_not_found if @incident.blank?
+    end
+    
+        private
+    
+    def incident_params
+        params.require(:incident).permit(:strategies, :description, :type)
+    end
 end
