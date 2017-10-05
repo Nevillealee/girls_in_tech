442270454577ac217c1a2e8df119554e55dadabf
nevillealee <nevillealee@gmail.com>
2017-10-05 06:41:20 +0000

    adds grid to search block

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,7 @@
 class Student < ApplicationRecord
     has_many :incidents
     
+    #search method for finding student by name field(first,last)"
     def self.search(search)
         where("lower(name) LIKE lower(?)", "%#{search}%")
     end
