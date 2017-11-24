12524425c17c1ed4e0ce8597c46580ede53c4e42
nevillealee <nevillealee@gmail.com>
2017-11-24 06:15:14 +0000

    adds has_many through challenges associations to students and incident_reports models

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,7 @@
 class Student < ApplicationRecord
     has_many :incident_reports, dependent: :destroy
     has_many :challenges, dependent: :destroy
+    has_many :incident_reports, :through => challenges
     
     #search method for finding student by name field(first,last)"
     def self.search(search)
