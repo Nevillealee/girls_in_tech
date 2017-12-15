411bff01d72142f9feb5e41456950faea030c36b
nevillealee <nevillealee@gmail.com>
2017-12-15 08:04:08 +0000

    writes rspec tests for student#edit

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -5,8 +5,14 @@ RSpec.describe StudentsController, type: :controller do
         it "should successfully render page after authenticated user signs in" do
             user = FactoryGirl.create(:user)
             sign_in user
-            
             get :index
+            expect(response).to have_http_status(:success)
+        end
+    end
+    
+    describe "students#edit action" do
+        it "should render edit page with form for updating changes to student model" do
+            get :edit
             expect(response).to have_http_status(:success)
         end
     end
